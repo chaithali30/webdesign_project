bd3d1da07fce75836e34f34a777cb34b8df4715f
chaithaligowda <chaithali@digitalprizm.net>
2024-02-22 09:43:45 +0530

    pushing to github

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.postcss";
+  import Header from "../lib/component/Header.svelte";
+</script>
+
+
+<div class="">
+  <Header></Header>
+<!-- -->
+<div class="w-full">
+  <slot /> 
+</div>
+</div>
